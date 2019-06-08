bce4fb7a60f7e58e585f398e3705b4aac1b4593b
somen0101 <so_men0101@outlook.jp>
2019-06-09 02:19:18 +0900

    保存した配列ちゅいか

--- TheAnswer/url_creater.py
+++ TheAnswer/url_creater.py
@@ -7,7 +7,8 @@ Created on Fri Jun  7 18:37:17 2019
 
 from bs4 import BeautifulSoup
 import requests
-
+import pandas as pd
+import numpy as np
 
 def URL_CREATER(x):#　x=西暦
     array=[]
@@ -16,7 +17,7 @@ def URL_CREATER(x):#　x=西暦
             SCH_URL='http://npb.jp/games/'+str(x)+'/schedule_'+str(0)+str(l)+'_detail.html'
             r = requests.get(SCH_URL)
             r.encoding = r.apparent_encoding
-            soup = BeautifulSoup(r.text)
+            soup = BeautifulSoup(r.text,'html5lib')
             for k in range(int(l)*100+1,int(l)*100+32):
                 a="date0"+str(k)
                 d = soup.find_all(id=a)
@@ -26,15 +27,13 @@ def URL_CREATER(x):#　x=西暦
             SCH_URL='http://npb.jp/games/'+str(x)+'/schedule_'+str(l)+'_detail.html' 
             r = requests.get(SCH_URL)
             r.encoding = r.apparent_encoding
-            soup = BeautifulSoup(r.text)
+            soup = BeautifulSoup(r.text,'html5lib')
             for k in range(int(l)*100+1,int(l)*100+32):
                 a="date"+str(k)
                 d = soup.find_all(id=a)
                 for m in range(0,len(d)):
                     if "勝：" in str(d[m]) or "分：" in str(d[m]):array.append( "https://spaia.jp/baseball/npb/game/"+str(x)+str(k)+str(0)+str(m+1))
-                
-         
-    #df = pd.DataFrame(array)       
     return array
-       
+    #np.save("bb_URLs_20190609",array) #配列保存
+
 print(URL_CREATER(2019))
