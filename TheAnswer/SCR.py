062f736131fe0e8de9ceee17b5be11bc57b0ef63
somen0101 <so_men0101@outlook.jp>
2019-06-09 02:53:01 +0900

    実行ファイルちゅいか

---
+++ TheAnswer/SCR.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 02:20:21 2019
+
+@author: 81802
+"""
+import numpy as np
+import baseballscrape
+
+data = np.load(file="bb_URLs_20190609.npy",)#URL配列ファイル読み込み
+for i in(0,len(data)-1):
+    bbd = baseballscrape.BBdata(data[i])
+    bbd.scr_gamedeta()
